6e49e288f0ae11359a1a6e5878b174a39f64a7d8
Manish Sahu <manissh7352@gmail.com>
2022-06-04 21:32:15 +0530

    changes fixed

--- src/sharedFolder/Dashboard/Analytics & Reports/analytics_Card.jsx
+++ src/sharedFolder/Dashboard/Analytics & Reports/analytics_Card.jsx
@@ -1,56 +1,51 @@
 import React from "react";
 import "./analytics_card.css";
 import { AiOutlineCaretUp, AiOutlineCaretDown } from "react-icons/ai";
-import circle  from '../../../Asset/circle.png'
-import totalMember from '../../../Asset/totalMember.png'
+import circle from "../../../Asset/circle.png";
+import totalMember from "../../../Asset/totalMember.png";
 import activeContracts from "../../../Asset/activeContracts.png";
 
 const analytics_Card = () => {
-  return (<>
-    <div className="status-card">
-      <div className="status-card__icon">
-        Total Closed 
-        contracts
+  return (
+    <>
+      <div className="status-card">
         <div className="status-card__icon">
-        68  <AiOutlineCaretUp />
+          Total Closed contracts
+          <div className="status-card__icon">
+            68 <AiOutlineCaretUp />
+          </div>
+        </div>
+        <div className="status-card__info">
+          <img src={circle} alt="circle" className="stat-img" />
         </div>
       </div>
-      <div className="status-card__info">
-        <img src={circle} alt='circle' className="stat-img" />
-      </div>
-    </div>
-    <div className="status-card">
-      <div className="status-card__icon">
-        <p>   562 </p>
-        <p>Total Members </p><AiOutlineCaretUp />
-        <p>-2% than last month</p>
-        
-      </div>
-      <div className="status-card__info">
-      <img src={totalMember} alt='total-member' className="stat-img"/>
-      </div>
-    </div>
-    <div className="status-card">
-      <div className="status-card__icon">
-        Ongoing <br />
-        contracts
-      </div>
-      <div className="status-card__info">76 left from target</div>
-    </div>
-    <div className="status-card">
-      <div className="status-card__icon">
-        <h6>892</h6>
-        <h6>active
-        contracts</h6>
-
+      <div className="status-card">
         <div className="status-card__icon">
-          <img src={activeContracts} alt='active' className="stat-img"/>
+          <p> 562 </p>
+          <p>Total Members </p>
+          <AiOutlineCaretUp />
+          <p>-2% than last month</p>
+        </div>
+        <div className="status-card__info">
+          <img src={totalMember} alt="total-member" className="stat-img" />
         </div>
       </div>
-      <div className="status-card__info">
-      
+      <div className="status-card">
+        <div className="status-card__icon">
+          Ongoing <br />
+          contracts
+        </div>
+        <div className="status-card__info">76 left from target</div>
       </div>
-    </div>
+      <div className="status-card">
+        <div className="status-card__icon">
+          Progress
+          <div className="status-card__icon">
+            {/* <img src={activeContracts} alt='active' className="stat-img"/> */}
+          </div>
+        </div>
+        <div className="status-card__info"></div>
+      </div>
     </>
   );
 };
